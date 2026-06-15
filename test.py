1747da17243385b6165fc0153f99631465c38739
mohameddessamm <jokermohamed923@gmail.com>
2026-06-15 16:25:45 +0300

    add file csv

--- test.py
+++ test.py
@@ -29,8 +29,28 @@ files = "txts.txt"
 # content = file.read()
 # print(content.rstrip())
 
-with open (files) as filess:
-    lcounter = 1
-    for line in filess:
-        print(lcounter, line.rstrip())
-        lcounter  += 1
+# with open (files) as filess:
+#     lcounter = 1
+#     for line in filess:
+#         print(lcounter, line.rstrip())
+#         lcounter  += 1
+
+# الطبقة 0: (على الحيطة خالص بره)
+
+
+# الطبقة 0: البوابة الأولى بتبدأ من أول السطر خالص
+with open(files, "a") as files_object:
+    # الطبقة 1: دخلنا جوة الـ with (اضغط Tab واحدة)
+    add_line = files_object.write(" hi am mohamed essam2 \n ") 
+
+# الطبقة 0: رجعنا تاني بره خالص عشان نفتح البوابة الثانية
+with open(files, "r") as object_files2:
+    # الطبقة 1: دخلنا جوة الـ with الثانية (اضغط Tab واحدة)
+    count = 1
+    content_file = object_files2.readlines()
+    
+    # الطبقة 1: الـ for نفسها جوا الـ with فتأخد (Tab واحدة)
+    for all_lines in content_file:
+        # الطبقة 2: السطور دي جوه الـ for وجوه الـ with (اضغط 2 Tab)
+        print(count, all_lines.rstrip())
+        count += 1
